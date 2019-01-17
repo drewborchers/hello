c4aa15ff11789a664a0211d7bdc99da151186d3d
drewborchers <46796493+drewborchers@users.noreply.github.com>
2019-01-17 14:41:11 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Drew Borchers & Trevor Thelander
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Drew Borchers & Trevor Thelander\n");
+
+  return 0;
+}
